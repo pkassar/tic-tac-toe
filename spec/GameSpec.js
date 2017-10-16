d0b4f9bad051d0f3d34d582de1060098594a545e
Paul Kassar <paulkassar@gmail.com>
2017-10-16 16:20:54 +0100

    jquery

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,9 +11,5 @@ describe('Game', function(){
     it('has a game board array', function(){
       expect(game.board).toEqual(['', '', '', '', '', '', '', '', ''])
     })
-
-    // it('starts with player O', function(){
-    //   expect(game.turn).toEqual('O')
-    // })
   })
 })
